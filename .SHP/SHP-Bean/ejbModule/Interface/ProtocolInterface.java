35071fe559acf8c1f295dbc2368c792ee84b67ca
jowieweb <jowieweb@gmail.com>
2017-06-29 09:20:05 +0200

    kommentar eingefügt

--- .SHP/SHP-Bean/ejbModule/Interface/ProtocolInterface.java
+++ .SHP/SHP-Bean/ejbModule/Interface/ProtocolInterface.java
@@ -1,12 +1,42 @@
 package Interface;
 
+/**
+ * @author Jonas
+ * Abstract class for protocol handler
+ * A protocol handler connects to a "broker". It can send and receive messages from the nodes
+ */
 public abstract class ProtocolInterface {
+	
 	protected Receiver r = null;
 	protected ProtocolInterface(Receiver r){
 		this.r = r;
 	}
+	
+	/**
+	 * connect to the broker 
+	 * @throws Exception
+	 */
 	public abstract void connectToBroker() throws Exception;
+	
+	/**
+	 * publish a message to the broker
+	 * @param t the "topic"
+	 * @param message the message
+	 * @return false = failed
+	 */
 	public abstract boolean publish(String t, String message);
+	
+	/**
+	 * start listening to the topic
+	 * @param t the topic
+	 * @throws Exception
+	 */
 	public abstract void listento(String t) throws Exception;
+	
+	/**
+	 * start listening to the following topics ONLY. 
+	 * @param t
+	 * @throws Exception
+	 */
 	public abstract void listento(String[] t) throws Exception;
 }
